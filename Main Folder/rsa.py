cc12d90d4446d573169d1b8d729fa12579ddbdd0
Aliza Alfarisi <jongleejong32@email.com>
2026-09-26 11:12:41 +0700

    auto-sync: 2026-09-26 11:12:41 [Aliza Alfarisi]

--- Main Folder/rsa.py
+++ Main Folder/rsa.py
@@ -88,7 +88,8 @@ st.caption("Enkripsi & Dekripsi asimetris — kunci publik untuk mengenkripsi, k
 if "rsa_key" not in st.session_state:
     st.session_state.rsa_key = None
 
-#
+#pembangkitan kunci (input p dan q manual)
+
 st.header("Pembangkitan Kunci")
 
 col1, col2 = st.columns(2)
